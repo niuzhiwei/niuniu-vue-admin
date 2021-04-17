25b96464a87f1ebe748974c2ae6f367c47f87da7
niuzhiwei <xujin02@qq.com>
2021-04-17 15:27:57 +0800

    修复bug

--- src/components/Tinymac/loadScript.js
+++ src/components/Tinymac/loadScript.js
@@ -1,17 +1,18 @@
 let callbacks = []
 
-function loadedTinymac() {
-    //检查tinymac是否加载完成
-    return window.tinymac
+function loadedTinymce() {
+    // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2144
+    // check is successfully downloaded script
+    return window.tinymce
 }
 
-const loadScript = (src, callback) => {
+const dynamicLoadScript = (src, callback) => {
     const existingScript = document.getElementById(src)
     const cb = callback || function () {}
 
     if (!existingScript) {
         const script = document.createElement('script')
-        script.src = src
+        script.src = src // src url for the third-party library being loaded.
         script.id = src
         document.body.appendChild(script)
         callbacks.push(cb)
@@ -20,39 +21,39 @@ const loadScript = (src, callback) => {
     }
 
     if (existingScript && cb) {
-        if (loadedTinymac()) {
+        if (loadedTinymce()) {
             cb(null, existingScript)
         } else {
             callbacks.push(cb)
         }
     }
-}
 
-function stdOnEnd(script) {
-    script.onload = function () {
-        // this.onload = null here is necessary
-        // because even IE9 works not like others
-        this.onerror = this.onload = null
-        for (const cb of callbacks) {
-            cb(null, script)
+    function stdOnEnd(script) {
+        script.onload = function () {
+            // this.onload = null here is necessary
+            // because even IE9 works not like others
+            this.onerror = this.onload = null
+            for (const cb of callbacks) {
+                cb(null, script)
+            }
+            callbacks = null
         }
-        callbacks = null
-    }
-    script.onerror = function () {
-        this.onerror = this.onload = null
-        cb(new Error('Failed to load ' + src), script)
-    }
-}
-
-function ieOnEnd(script) {
-    script.onreadystatechange = function () {
-        if (this.readyState !== 'complete' && this.readyState !== 'loaded') return
-        this.onreadystatechange = null
-        for (const cb of callbacks) {
-            cb(null, script) // there is no way to catch loading errors in IE8
+        script.onerror = function () {
+            this.onerror = this.onload = null
+            cb(new Error('Failed to load ' + src), script)
         }
-        callbacks = null
+    }
+
+    function ieOnEnd(script) {
+        script.onreadystatechange = function () {
+            if (this.readyState !== 'complete' && this.readyState !== 'loaded') return
+            this.onreadystatechange = null
+            for (const cb of callbacks) {
+                cb(null, script) // there is no way to catch loading errors in IE8
+            }
+            callbacks = null
+        }
     }
 }
 
-export default loadScript
+export default dynamicLoadScript
